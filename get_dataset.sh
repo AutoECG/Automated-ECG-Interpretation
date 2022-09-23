bc083d65996a71aab78b040fac087b0c746edfa7
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-09-23 11:24:33 +0300

    Add no-check-certificate

--- get_dataset.sh
+++ get_dataset.sh
@@ -6,7 +6,7 @@
 
 mkdir -p data
 cd data || return
-wget https://storage.googleapis.com/ptb-xl-1.0.1.physionet.org/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1.zip
+wget https://storage.googleapis.com/ptb-xl-1.0.1.physionet.org/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1.zip --no-check-certificate
 unzip ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1.zip
 mv ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1 ptbxl
 cd ..
