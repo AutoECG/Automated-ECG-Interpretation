a749ef4f721e33e938fb6949a8eac35f15ba93df
Zaki Kurdya <46399191+ZakiKurdya@users.noreply.github.com>
2022-09-23 10:05:55 +0300

    Add get_dataset.sh

---
+++ get_dataset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+####################################
+#   GET PTB-XL DATABASE
+####################################
+
+mkdir -p data
+cd data || return
+wget https://storage.googleapis.com/ptb-xl-1.0.1.physionet.org/ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1.zip
+unzip ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1.zip
+mv ptb-xl-a-large-publicly-available-electrocardiography-dataset-1.0.1 ptbxl
+cd ..
